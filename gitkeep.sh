3b5e5760c512906d83489734dcc6b4eee775ccf6
luemonkey123 <luemonkey123@gmail.com>
2025-03-28 20:22:35 -0400

    Dockerfile

--- gitkeep.sh
+++ gitkeep.sh
@@ -1,4 +1,11 @@
+rm -rf temp
+mkdir temp
+mkdir temp/audio
+mkdir temp/text
+mkdir temp/thumbnail
 touch ./temp/audio/.gitkeep
 touch ./temp/text/.gitkeep
 touch ./temp/thumbnail/.gitkeep
+rm -rf output-files
+mkdir output-files
 touch ./output-files/.gitkeep
